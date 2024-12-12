d1351970f93c9ce99f28fe5ad7b47dfc9e12fab8
zoykh1 <kerimgmc45@gmail.com>
2024-12-12 08:46:10 +0100

    trying to fix the problem

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -1,14 +1,44 @@
 #include "binary_trees.h"
 
 /**
+ * height - Computes the height of a binary tree.
+ * @tree: A pointer to the root node of the tree.
+ *
+ * Return: The height of the tree.
+ */
+int height(const binary_tree_t *tree)
+{
+	if (tree == NULL)
+		return (0);
+	return (1 + (max(height(tree->left), height(tree->right))));
+}
+
+/**
+ * max - Returns the maximum of two integers.
+ * @a: The first integer.
+ * @b: The second integer.
+ *
+ * Return: The maximum of a and b.
+ */
+int max(int a, int b)
+{
+	return ((a > b) ? a : b);
+}
+
+/**
  * binary_tree_balance - Measures the balance factor of a binary tree.
- * @tree: Pointer to the root node of the tree to measure the balance factor.
- * Return: The balance factor of the tree. If the tree is NULL, returns 0.
+ * @tree: A pointer to the root node of the tree.
+ *
+ * Return: The balance factor of the tree.
+ *         0 if tree is NULL.
  */
 int binary_tree_balance(const binary_tree_t *tree)
 {
 	if (tree == NULL)
 		return (0);
 
-	return (binary_tree_height(tree->left) - binary_tree_height(tree->right));
+	int left_height = height(tree->left);
+	int right_height = height(tree->right);
+
+	return (left_height - right_height);
 }
